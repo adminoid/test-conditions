36b6505918b185ed095e991ef385ee04c3194211
Petr Zenin <mr@adminoid.com>
2020-02-05 05:23:00 +0300

    added card type functionality

--- components/SubConditionType.vue
+++ components/SubConditionType.vue
@@ -1,3 +1,33 @@
 <template lang="pug">
-  div SubConditionType
+  div
+    b-form(inline)
+      b-form-select(v-model="selected" :options="options")
+      b-button(variant="danger" @click="erase")
+        b-icon-trash
 </template>
+<script>
+import { BIcon, BIconTrash } from 'bootstrap-vue'
+export default {
+  name: 'SubConditionType',
+  components: {
+    BIcon,
+    BIconTrash
+  },
+  data () {
+    return {
+      selected: null,
+      options: [
+        { value: null, text: 'select card type' },
+        { value: 'gold', text: 'Gold' },
+        { value: 'silver', text: 'Silver' },
+        { value: 'copper', text: 'Copper' }
+      ]
+    }
+  },
+  methods: {
+    erase () {
+      this.$emit('delete-sub-condition', { idxToDelete: this.$vnode.key })
+    }
+  }
+}
+</script>
