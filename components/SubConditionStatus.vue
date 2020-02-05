772dc6c0e9db0ce240a0271b3aa2937ec43557fa
Petr Zenin <mr@adminoid.com>
2020-02-05 05:19:00 +0300

    added card status functionality

--- components/SubConditionStatus.vue
+++ components/SubConditionStatus.vue
@@ -1,3 +1,31 @@
 <template lang="pug">
-  div SubConditionStatus
+  div
+    b-form(inline)
+      b-form-select(v-model="selected" :options="options")
+      b-button(variant="danger" @click="erase")
+        b-icon-trash
 </template>
+<script>
+import { BIcon, BIconTrash } from 'bootstrap-vue'
+export default {
+  name: 'SubConditionStatus',
+  components: {
+    BIcon,
+    BIconTrash
+  },
+  data () {
+    return {
+      selected: null,
+      options: [
+        { value: null, text: 'disabled' },
+        { value: 'active', text: 'Active' }
+      ]
+    }
+  },
+  methods: {
+    erase () {
+      this.$emit('delete-sub-condition', { idxToDelete: this.$vnode.key })
+    }
+  }
+}
+</script>
